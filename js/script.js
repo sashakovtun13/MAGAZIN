4bad477e55ab9175bd23cc5cb4c49570170a23d3
Aleks <sashakovtun13@gmail.com>
2020-01-08 12:39:19 +0200

    update_script + comment

--- js/script.js
+++ js/script.js
@@ -10,19 +10,28 @@
      $.getJSON("base/goods.json",goodGet);
    }
   //Сама функция считывания
-  function goodGet(date){
-    console.log(date);
+  function goodGet(data){
+    console.log(data);
 
     var out=" ";
-    for(var k in date){
-      out+='<div class="cart">';
-      
-      out+=`<img src="base/${date[k].good_img}" alt="" class="good_img">`;
-      out+=`<h1 class="name">${date[k].name}</h1>`;
-      out+=`<p class="cost">${date[k].cost} грн.</p>`;
-      out+=`<p class="desc">${date[k].desc}</p>`;
+    for(var k in data){
+        out +='<div class="cart">';
+        
+      out+='<img src="base/'+data[k].good_img+'" alt="" class="good_img">';
+      out+='<h1 class="name">'+data[k].name+'</h1>';
+      out+='<p class="cost">'+data[k].cost+' грн.</p>';
+      out+='<p class="desc">'+data[k].desc+'</p>';
       out+='<button>Купить</button>';
-      out+='</div>';
+        out +='</div>';
+
+      // out+='<div class="cart">';
+      
+      // out+=`<img src="base/${data[k].good_img}" alt="" class="good_img">`;
+      // out+=`<h1 class="name">${data[k].name}</h1>`;
+      // out+=`<p class="cost">${data[k].cost} грн.</p>`;
+      // out+=`<p class="desc">${data[k].desc}</p>`;
+      // out+='<button>Купить</button>';
+      // out+='</div>';
     }
     //Вывод на экран
     $(".good-out").html(out);
