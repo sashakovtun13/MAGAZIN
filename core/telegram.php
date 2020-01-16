db0ce019beac5a07ab1dc26b5d68682be4e19c17
Aleks <sashakovtun13@gmail.com>
2020-01-16 14:17:51 +0200

    php

---
+++ core/telegram.php
@@ -0,0 +1,35 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "";
+$chat_id = "";
+
+
+
+
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Заказ:',
+  
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
+
